a322e2db51af70aca68d67f1baf261c0a7250e19
silverain <ekdldkaa@naver.com>
2025-04-15 14:15:09 +0900

    feat : add fail test case

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -1,7 +1,8 @@
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import SignupPage from '../pages/SignupPage';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@testing-library/jest-dom';
 
 const queryClient = new QueryClient({
   defaultOptions: {},
@@ -28,5 +29,16 @@ describe('회원가입 테스트', () => {
         <RouterProvider router={router} />
       </QueryClientProvider>,
     );
+
+    //when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
+    const passwordInput = screen.getByLabelText('비밀번호');
+    const confirmPasswordInput = screen.getByLabelText('비밀번호 확인');
+
+    fireEvent.change(passwordInput, { target: { value: 'password' } });
+    fireEvent.change(confirmPasswordInput, { target: { value: 'wrongPassword' } });
+
+    //then - 에러 메시지가 표시됨
+    const errorMessage = await screen.findByTestId('error-message');
+    expect(errorMessage).toBeInTheDocument();
   });
 });
