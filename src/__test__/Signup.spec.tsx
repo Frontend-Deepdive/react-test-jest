ab26eb2a60d13a71956b53bfe71b677ce126f0e2
neungdong <iyeseul877@gmail.com>
2025-04-17 16:56:05 +0900

    test(signup): 회원가입 페이지 렌더링 테스트 준비 단계(given)

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -1,3 +1,38 @@
-describe("회원가입 테스트", () => {
-  test("환경 설정 테스트", () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import SignupPage from '../pages/SignupPage';
+import '@testing-library/jest-dom'; 
+
+describe('회원가입 테스트', () => {
+  // given : 테스트 실행을 위한 환경 구성 
+  beforeEach(() => {
+    const queryClient = new QueryClient();
+
+    // 테스트용 라우터 설정 
+    const routes = [
+      {
+        path: '/signup',
+        element: <SignupPage />,
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ['/signup'], // 초기 진입 경로
+    });
+
+    // 테스트를 위한 컴포넌트 렌더링 
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    );
+  });
+
+  test('회원가입 페이지가 렌더링된다', () => {
+    const emailInput = screen.getByPlaceholderText('이메일을 입력해주세요');
+    expect(emailInput).toBeInTheDocument(); 
+  });
 });
+
